83a556f8f8928416350d464a88c9499a3ac5e138
Prabhat Gokul <prabhatgokul@stephansmbp620.globalmouse.local>
2018-08-31 14:43:23 +0200

    Implement iPhoneX image

--- js/discovery-image-hotspot-poc.js
+++ js/discovery-image-hotspot-poc.js
@@ -1,13 +1,25 @@
-$(document).ready(function () {
+jQuery(document).ready(function($) {
     console.log("document ready");
+    if (isMobile()) {
+        // console.log("is mobile so move popups form mobile");
+        movePopupsForMobile();
+    }
+
+    $(window).resize(function () {
+        if (isMobile()) {
+            closeAllPopups();
+            movePopupsForMobile();
+        }
+    });
+
     $(".pulse").click(function () {
 
         if (isMobile()) {
             if ($(this).find("i").hasClass("ti-plus")) {
-                closeAllOtherPopups(this);
+                closeAllPopups();
                 togglePopupFromPulse(this);
             } else {
-                closeAllOtherPopups(this);
+                closeAllPopups(this);
             }
         } else {
             togglePopupFromPulse(this);
@@ -20,8 +32,11 @@ $(document).ready(function () {
     });
 
     $(".hs-popup>.header>i").click(function () {
-        $(this).parent().parent().fadeOut();
-        $(this).parent().parent().parent().find(".pulse").find("i").toggleClass("ti-plus").toggleClass("ti-minus");
+        var popupContainer = $(this).parent().parent();
+        var popupId = $(popupContainer).attr("data-popup-id");
+        var pulseSelector = `.pulse[data-popup-id="${popupId}"]`;
+        $(popupContainer).fadeOut();
+        $(pulseSelector).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
     });
 
     $(".accordion-toggle").click(function () {
@@ -29,20 +44,38 @@ $(document).ready(function () {
         $(this).next().slideToggle("fast");
     });
 
-    function closeAllOtherPopups(currentPulse) {
-        $(".hs-popup").fadeOut();
+    function closeAllPopups() {
+        $(".hs-popup").hide();
         $(".pulse").find("i").removeClass("ti-minus").addClass("ti-plus");
     }
 
     function togglePopupFromPulse(currentPulse) {
-        if ($(currentPulse).find("i").hasClass("ti-plus"))
-            $(currentPulse).next().fadeIn();
-        else
-            $(currentPulse).next().fadeOut();
+        var popupId = $(currentPulse).attr("data-popup-id");
+        var popupSelector = `.hs-popup[data-popup-id="${popupId}"]`;
+        // console.log("popup id " + popupId);
+        // console.log("popup selector " + popupSelector);
+        // console.log($(popupSelector).hasClass("hidden"));
+        if ($(currentPulse).find("i").hasClass("ti-plus")) {
+            $(popupSelector).fadeIn();
+            // $(popupSelector).animate({
+            //     width: "toggle"
+            // });
+        } else
+            $(popupSelector).fadeOut();
         $(currentPulse).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
     }
 
     function isMobile() {
         return window.matchMedia('(max-width: 768px)').matches;
+    }
+
+    function movePopupsForMobile() {
+        $(".hs-popup").each(function () {
+            var parent2 = $(this).parents(".image-overlay");
+            var currentEl = $(this).detach();
+            // console.log(parent2);
+            // console.log(currentEl);
+           $(parent2).append(currentEl);
+        });
     }
 });
