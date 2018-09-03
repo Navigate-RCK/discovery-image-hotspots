37b351d230fcba36edb3069d19567b16dbd5a154
Prabhat Gokul <prabhatgokul@stephansmbp620.globalmouse.local>
2018-09-03 16:19:52 +0200

    Implement image-hotspot-vitality - design from Ivan + begin prototyping health-app concept

--- js/discovery-image-hotspot-poc.js
+++ js/discovery-image-hotspot-poc.js
@@ -1,21 +1,40 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     console.log("document ready");
     if (isMobile()) {
         // console.log("is mobile so move popups form mobile");
         movePopupsForMobile();
     }
 
-    $(window).resize(function () {
-        if (isMobile()) {
-            closeAllPopups();
-            movePopupsForMobile();
-        }
-    });
+    // $(window).resize(function () {
+    //     if (isMobile()) {
+    //         closeAllPopups();
+    //         movePopupsForMobile();
+    //     }
+    // });
 
-    $(".pulse").click(function () {
+    // $(".pulse").click(function () {
+    //
+    //     if (isMobile()) {
+    //         if ($(this).find("i").hasClass("ti-plus")) {
+    //             closeAllPopups();
+    //             togglePopupFromPulse(this);
+    //         } else {
+    //             closeAllPopups(this);
+    //         }
+    //     } else {
+    //         togglePopupFromPulse(this);
+    //     }
+    //     // var pulse = this;
+    //     // var offset = $(pulse).offset();
+    //     // console.log(offset);
+    //     // console.log($(window).scrollTop());
+    //     // $(this).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
+    // });
+
+    $(".pulse-vitality").click(function () {
 
         if (isMobile()) {
-            if ($(this).find("i").hasClass("ti-plus")) {
+            if (!$(this).hasClass("pulse-vitality-selected")) {
                 closeAllPopups();
                 togglePopupFromPulse(this);
             } else {
@@ -31,22 +50,44 @@ jQuery(document).ready(function($) {
         // $(this).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
     });
 
+    // $(".pulse-vitality").hover(
+    //     function () {
+    //         console.log(".pulse-vitality hover in");
+    //         var popupId = $(this).attr("data-popup-id");
+    //         var popupSelector = `.hs-popup[data-popup-id="${popupId}"]`;
+    //         $(popupSelector).fadeIn();
+    //     }/*,
+    //     function () {
+    //         console.log(".pulse-vitality hover out");
+    //         var popupId = $(this).attr("data-popup-id");
+    //         var popupSelector = `.hs-popup[data-popup-id="${popupId}"]`;
+    //         $(popupSelector).fadeOut();
+    //     }*/
+    // );
+
+    $(".hs-popup").click(function () {
+
+    });
+
     $(".hs-popup>.header>i").click(function () {
         var popupContainer = $(this).parent().parent();
         var popupId = $(popupContainer).attr("data-popup-id");
-        var pulseSelector = `.pulse[data-popup-id="${popupId}"]`;
+        var pulseSelector = `.pulse-vitality[data-popup-id="${popupId}"]`;
         $(popupContainer).fadeOut();
-        $(pulseSelector).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
+        $(pulseSelector).toggleClass("pulse-vitality-selected");
     });
 
     $(".accordion-toggle").click(function () {
-        $(this).find("i").toggleClass("ti-angle-up").toggleClass("ti-angle-down");
-        $(this).next().slideToggle("fast");
+        $(this).closest(".footer").prev().find(".accordion-container").slideToggle("fast");
+        $(this).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
+        $(this).closest(".footer").prev().find(".heading-divider").toggle();
+        // $(this).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
+        // $(this).next().slideToggle("fast");
     });
 
     function closeAllPopups() {
         $(".hs-popup").hide();
-        $(".pulse").find("i").removeClass("ti-minus").addClass("ti-plus");
+        $(".pulse-vitality").removeClass("pulse-vitality-selected");
     }
 
     function togglePopupFromPulse(currentPulse) {
@@ -55,14 +96,15 @@ jQuery(document).ready(function($) {
         // console.log("popup id " + popupId);
         // console.log("popup selector " + popupSelector);
         // console.log($(popupSelector).hasClass("hidden"));
-        if ($(currentPulse).find("i").hasClass("ti-plus")) {
+        if (!$(currentPulse).hasClass("pulse-vitality-selected")) {
             $(popupSelector).fadeIn();
             // $(popupSelector).animate({
             //     width: "toggle"
             // });
-        } else
+        } else {
             $(popupSelector).fadeOut();
-        $(currentPulse).find("i").toggleClass("ti-plus").toggleClass("ti-minus");
+        }
+        $(currentPulse).toggleClass("pulse-vitality-selected");
     }
 
     function isMobile() {
@@ -75,7 +117,7 @@ jQuery(document).ready(function($) {
             var currentEl = $(this).detach();
             // console.log(parent2);
             // console.log(currentEl);
-           $(parent2).append(currentEl);
+            $(parent2).append(currentEl);
         });
     }
 });
